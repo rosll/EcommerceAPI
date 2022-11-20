1e1d46c421d8cd1fa953d7a5970ba4588e85850d
Romulo Lima <romulosilva-07@hotmail.com>
2022-11-20 03:54:28 -0300

    validations for entity Person

---
+++ EcommerceAPI.Domain/Entities/Person.cs
@@ -0,0 +1,45 @@
+﻿using EcommerceAPI.Domain.Validations;
+using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EcommerceAPI.Domain.Entities
+{
+    public sealed class Person
+    {
+        public int Id { get; private set; }
+        public string Name { get; private set; }
+        public string Document { get; private set; }
+        public string Phone { get; private set; }
+
+        // Constructor para validar
+        public Person(string name, string document, string phone)
+        {
+            Validation(name, document, phone);
+        }
+
+        // Constructor para editar
+        public Person(int id, string name, string document, string phone)
+        {
+            DomainValidationException.When(id < 0, "Id deve ser maior que 0");
+
+            Id = id;
+
+            Validation(name, document, phone);
+        }
+
+        private void Validation(string name, string document, string phone)
+        {
+            DomainValidationException.When(string.IsNullOrEmpty(name), "Nome deve ser informado!");
+            DomainValidationException.When(string.IsNullOrEmpty(document), "Documento deve ser informado!");
+            DomainValidationException.When(string.IsNullOrEmpty(phone), "Celular deve ser informado!");
+
+            Name = name;
+            Document = document;
+            Phone = phone;
+        }
+    }
+}
